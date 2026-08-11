fce92e555441fe292144918648a028e3b5d78bf2
Nick Robinett <115662956+nrnsitecore@users.noreply.github.com>
2026-08-11 10:35:50 -0500

    updated for ch image optimization

--- examples/esl/next.config.ts
+++ examples/esl/next.config.ts
@@ -31,6 +31,12 @@ const nextConfig: NextConfig = {
         hostname: 'xmc-*.**',
         port: '',
       },
+      {
+        protocol: 'https',
+        hostname: 'esl.sitecoresandbox.cloud',
+        port: '',
+        pathname: '/**',
+      },
     ],
     // Optimize image sizes for responsive loading
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
